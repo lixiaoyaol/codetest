afb09bfaa01496a55f7f21b417a9e74c42834b12
libofeng <bf_li@qq.com>
2024-07-22 16:26:13 +0800

    Update

--- fortran/test3.f90
+++ fortran/test3.f90
@@ -3,11 +3,12 @@ include 'write_arr_mat.f90'
 program main
     use m1, only: write_arr_mat
     implicit none
-    integer :: a1(4), a2(9), bb(2,2), cc(3,3)
+    integer :: a1(4),  bb(2,2)
+    real(8) :: a2(9), cc(3,3)
     integer :: i
 
     a1 = [(i, i=1,4)]
-    a2 = [(i, i=1,9)]
+    a2 = [1.d0, 2.d0, 3.d0, 4.d0, 5.d0, 6.d0, 7.d0, 8.d0, 9.d0]
     bb = reshape(a1, [2,2], order=[1,2])
     cc = reshape(a2, [3,3], order=[1,2])
 
