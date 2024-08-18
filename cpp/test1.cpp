36532440571f55e408b9200a31561b4ad9ea2f82
libofeng <bf_li@qq.com>
2024-08-18 14:46:23 +0800

    Update

--- cpp/test1.cpp
+++ cpp/test1.cpp
@@ -14,10 +14,15 @@ double df(double x) {
 
 int main()
 {
-    double x0 = 1.0;
+    double x0 = -7.0;
     double toler = 1e-6;
-    while (abs(f(x0)) > toler) {
+    int itermax = 100;
+
+    for(int i=0; i<itermax; i++) {
         x0 = x0 - f(x0) / df(x0);
+        if(abs(f(x0)) < toler) {
+            break;
+        }
         cout << fixed << setprecision(10) << x0 << endl;
     }
     cout << "f(x)= " << scientific << setprecision(6)  << f(x0) << endl;
