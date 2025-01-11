0eba07b8426c00c18777a3e8d21f5614fb7c7205
libofeng <bf_li@qq.com>
2025-01-11 17:59:30 +0800

    使用函数列出label和entry

--- python/tk.py
+++ python/tk.py
@@ -76,110 +76,69 @@ btn_kin.pack(side='left')
 btn_dmg.pack(side='left')
 frm3.pack(side='top', anchor='w')
 
+def gen_lab_ent(frm, fields, start_row=0):
+    for i, (lab_text, dft_val) in enumerate(fields.items()):
+        lab = tk.Label(frm, text=lab_text)
+        ent = tk.Entry(frm)
+        if dft_val:
+            ent.insert(0, dft_val)
+        lab.grid(row=(i+start_row), column=0, padx=10)
+        ent.grid(row=(i+start_row), column=1)
+
+
+def gen_trigger(frm, lab_text):
+    lab = tk.Label(frm, text=lab_text)
+    var = tk.IntVar()
+    btn = tk.Radiobutton(frm, text='Yes', variable=var, value=0)
+    btn2 = tk.Radiobutton(frm, text='No', variable=var, value=1)
+    lab.grid(row=0, column=0, padx=10)
+    btn.grid(row=0, column=1)
+    btn2.grid(row=0, column=2)
+    return var
+
 # UCP Parameters
-lab_ucp_aa = tk.Label(frm4, text='A')
-ent_ucp_aa = tk.Entry(frm4)
-lab_ucp_aa.grid(row=0, column=0, padx=10)
-ent_ucp_aa.grid(row=0, column=1)
-lab_ucp_bb = tk.Label(frm4, text='B')
-ent_ucp_bb = tk.Entry(frm4)
-lab_ucp_bb.grid(row=1, column=0, padx=10)
-ent_ucp_bb.grid(row=1, column=1)
-lab_ucp_nn = tk.Label(frm4, text='n')
-ent_ucp_nn = tk.Entry(frm4)
-lab_ucp_nn.grid(row=2, column=0, padx=10)
-ent_ucp_nn.grid(row=2, column=1)
-lab_ucp_dd = tk.Label(frm4, text='d')
-ent_ucp_dd = tk.Entry(frm4)
-lab_ucp_dd.grid(row=3, column=0, padx=10)
-ent_ucp_dd.grid(row=3, column=1)
-lab_ucp_qq = tk.Label(frm4, text='Q')
-ent_ucp_qq = tk.Entry(frm4)
-lab_ucp_qq.grid(row=4, column=0, padx=10)
-ent_ucp_qq.grid(row=4, column=1)
-lab_ucp_rr = tk.Label(frm4, text='R')
-ent_ucp_rr = tk.Entry(frm4)
-ent_ucp_rr.insert(0, '8.314')
-lab_ucp_rr.grid(row=5, column=0, padx=10)
-ent_ucp_rr.grid(row=5, column=1)
-lab_ucp_tt = tk.Label(frm4, text='T')
-ent_ucp_tt = tk.Entry(frm4)
-ent_ucp_tt.insert(0, '298')
-lab_ucp_tt.grid(row=6, column=0, padx=10)
-ent_ucp_tt.grid(row=6, column=1)
+fields_ucp = {
+    'A': None,
+    'B': None,
+    'n': None,
+    'd': None,
+    'Q': None,
+    'R': 8.314,
+    'T': 298
+}
+gen_lab_ent(frm4, fields_ucp)
 frm4.pack(side='top', anchor='w', pady=10)
 
 
 # Isotropic hardening parameters
-lab_iso_trig = tk.Label(frm5, text='Enable Isotropic Hardening')
-var_iso_trig = tk.IntVar()
-btn_iso_trig = tk.Radiobutton(frm5, text='Yes', variable=var_iso_trig, value=0)
-btn_iso_trig2 = tk.Radiobutton(frm5, text='No', variable=var_iso_trig, value=1)
-lab_iso_trig.grid(row=0, column=0, padx=10)
-btn_iso_trig.grid(row=0, column=1)
-btn_iso_trig2.grid(row=0, column=2)
-lab_iso_r0 = tk.Label(frm5, text='R0')
-ent_iso_r0 = tk.Entry(frm5)
-lab_iso_r0.grid(row=1, column=0, padx=10)
-ent_iso_r0.grid(row=1, column=1)
-lab_iso_inf = tk.Label(frm5, text='Rinf')
-ent_iso_inf = tk.Entry(frm5)
-lab_iso_inf.grid(row=2, column=0, padx=10)
-ent_iso_inf.grid(row=2, column=1)
-lab_iso_rb = tk.Label(frm5, text='Rb')
-ent_iso_rb = tk.Entry(frm5)
-lab_iso_rb.grid(row=3, column=0, padx=10)
-ent_iso_rb.grid(row=3, column=1)
+fields_iso = {
+    'R0': None,
+    'Rinf': None,
+    'Rb': None
+}
+var_iso_trig = gen_trigger(frm5, 'Enable Isotropic Hardening')
+gen_lab_ent(frm5, fields_iso, 1)
 # frm5.pack(side='top', anchor='w')
 
 # Kinematic hardening parameters
-lab_iso_trig = tk.Label(frm6, text='Enable Kinematic Hardening')
-var_kin_trig = tk.IntVar()
-btn_kin_trig = tk.Radiobutton(frm6, text='Yes', variable=var_kin_trig, value=0)
-btn_kin_trig2 = tk.Radiobutton(frm6, text='No', variable=var_kin_trig, value=1)
-lab_iso_trig.grid(row=0, column=0, padx=10)
-btn_kin_trig.grid(row=0, column=1)
-btn_kin_trig2.grid(row=0, column=2)
-lab_kin_mu = tk.Label(frm6, text='mu')
-ent_kin_mu = tk.Entry(frm6)
-lab_kin_mu.grid(row=1, column=0, padx=10)
-ent_kin_mu.grid(row=1, column=1)
-lab_kin_beta = tk.Label(frm6, text='beta')
-ent_kin_beta = tk.Entry(frm6)
-lab_kin_beta.grid(row=2, column=0, padx=10)
-ent_kin_beta.grid(row=2, column=1)
+fields_kin = {
+    'mu': None,
+    'beta': None
+}
+var_kin_trig = gen_trigger(frm6, 'Enable Kinematic Hardening')
+gen_lab_ent(frm6, fields_kin, 1)
 # frm6.pack(side='top', anchor='w')
 
 # Damage parameters
-lab_dmg_trig = tk.Label(frm7, text='Enable Damage')
-var_dmg_trig = tk.IntVar()
-btn_dmg_trig = tk.Radiobutton(frm7, text='Yes', variable=var_dmg_trig, value=0)
-btn_dmg_trig2 = tk.Radiobutton(frm7, text='No', variable=var_dmg_trig, value=1)
-lab_dmg_trig.grid(row=0, column=0, padx=10)
-btn_dmg_trig.grid(row=0, column=1)
-btn_dmg_trig2.grid(row=0, column=2)
-lab_dmg_lambda = tk.Label(frm7, text='lambda')
-ent_dmg_lambda = tk.Entry(frm7)
-lab_dmg_lambda.grid(row=1, column=0, padx=10)
-ent_dmg_lambda.grid(row=1, column=1)
-lab_dmg_rho = tk.Label(frm7, text='rho')
-ent_dmg_rho = tk.Entry(frm7)
-lab_dmg_rho.grid(row=2, column=0, padx=10)
-ent_dmg_rho.grid(row=2, column=1)
-lab_dmg_ms = tk.Label(frm7, text='ms')
-ent_dmg_ms = tk.Entry(frm7)
-lab_dmg_ms.grid(row=3, column=0, padx=10)
-ent_dmg_ms.grid(row=3, column=1)
-lab_dmg_rr = tk.Label(frm7, text='R')
-ent_dmg_rr = tk.Entry(frm7)
-ent_dmg_rr.insert(0, '8.314')
-lab_dmg_rr.grid(row=4, column=0, padx=10)
-ent_dmg_rr.grid(row=4, column=1)
-lab_dmg_tt = tk.Label(frm7, text='T')
-ent_dmg_tt = tk.Entry(frm7)
-ent_dmg_tt.insert(0, '298')
-lab_dmg_tt.grid(row=5, column=0, padx=10)
-ent_dmg_tt.grid(row=5, column=1)
+fields_dmg = {
+    'lambda': None,
+    'rho': None,
+    'ms': None,
+    'R': 8.314,
+    'T': 298
+}
+var_dmg_trig = gen_trigger(frm7, 'Enable Damage')
+gen_lab_ent(frm7, fields_dmg, 1)
 # frm7.pack(side='top', anchor='w')
 
 # Load and save buttons
