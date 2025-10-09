e91cb22ec685cbad26c6a24cede386b045ee77c5
libofeng <bf_li@qq.com>
2025-10-09 14:45:59 +0800

    Update

--- fortran/test22.f90
+++ fortran/test22.f90
@@ -2,6 +2,8 @@ program gethomedir
     implicit none
     character(len=255) :: homedir
     integer :: status, lenhome
+    integer :: ios
+    character(:), allocatable :: filepath
 
     ! Get the home directory using the Fortran intrinsic function
     call get_environment_variable("USERPROFILE", homedir, lenhome, status, .true.)
@@ -15,4 +17,14 @@ program gethomedir
     elseif( status == -1) then
         write (*,*) 'env var length = ', lenhome, ' grater thatn 255'
     end if
+
+    filepath = 'E:/gitrepo/codetest/fortran/test22.f90'
+    open(unit=10, file=filepath, status='old', action='read', iostat=ios)
+    if (ios == 0) then
+        print *, 'File opened successfully: ', trim(filepath)
+        close(10)
+    else if (ios /= 0) then
+        print *, 'Error opening file: ', trim(filepath)
+    end if
+
 end program gethomedir
